57b1f864efdebbc0c8b2d3c19f180a9941997619
liuxu <244983740@qq.com>
2019-02-27 23:16:26 +0800

    client-consumer

---
+++ eureka-consumer/src/main/java/com/liuxu/springcloud/eurekaconsumer/DcController.java
@@ -0,0 +1,33 @@
+package com.liuxu.springcloud.eurekaconsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Administrator
+ */
+
+@RestController
+public class DcController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/dc")
+    public String dc() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+
+}
---
+++ eureka-consumer/src/main/java/com/liuxu/springcloud/eurekaconsumer/EurekaConsumerApplication.java
@@ -0,0 +1,25 @@
+package com.liuxu.springcloud.eurekaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Administrator
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
